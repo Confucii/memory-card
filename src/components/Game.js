db8c9439cd9e8aa874d7a40c1a51ed5616678392
Oleksii <lesha.avdeev@gmail.com>
2023-05-03 03:16:11 +0300

    Add score modification functions

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,11 +7,22 @@ export default function Game() {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
 
+  function incrementScore() {
+    setScore((s) => s + 1);
+  }
+
+  function newHighScore() {
+    if (highScore < score) {
+      setHighScore(score);
+    }
+    setScore(0);
+  }
+
   return (
     <div className="Game">
       <ScoreHolder string="Score" score={score} />
       <ScoreHolder string="High score" score={highScore} />
-      <CardHolder />
+      <CardHolder incrementScore={incrementScore} setHighScore={newHighScore} />
     </div>
   );
 }
