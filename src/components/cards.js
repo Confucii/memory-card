7be696c525d71babd4651f6a93046de362c3d4e2
Oleksii <lesha.avdeev@gmail.com>
2023-05-03 03:15:42 +0300

    Add clicked property to card

--- src/components/cards.js
+++ src/components/cards.js
@@ -12,18 +12,18 @@ import squirrel from "../images/squirrel.png";
 import weasel from "../images/weasel.png";
 
 const cards = [
-  { image: badger, name: "Badger" },
-  { image: caiman, name: "Caiman" },
-  { image: crocodile, name: "Crocodile" },
-  { image: dog, name: "Dog" },
-  { image: fennec, name: "Fennec" },
-  { image: fox, name: "Fox" },
-  { image: giraffe, name: "Giraffe" },
-  { image: lion, name: "Lion" },
-  { image: lynx, name: "Lynx" },
-  { image: raccoon, name: "Raccoon" },
-  { image: squirrel, name: "Squirrel" },
-  { image: weasel, name: "Weasel" },
+  { image: badger, name: "Badger", clicked: false },
+  { image: caiman, name: "Caiman", clicked: false },
+  { image: crocodile, name: "Crocodile", clicked: false },
+  { image: dog, name: "Dog", clicked: false },
+  { image: fennec, name: "Fennec", clicked: false },
+  { image: fox, name: "Fox", clicked: false },
+  { image: giraffe, name: "Giraffe", clicked: false },
+  { image: lion, name: "Lion", clicked: false },
+  { image: lynx, name: "Lynx", clicked: false },
+  { image: raccoon, name: "Raccoon", clicked: false },
+  { image: squirrel, name: "Squirrel", clicked: false },
+  { image: weasel, name: "Weasel", clicked: false },
 ];
 
 export default cards;
