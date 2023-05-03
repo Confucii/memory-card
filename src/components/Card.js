c967122054d1d6f77092a61fb1b6ce9f4e0a49f8
Oleksii <lesha.avdeev@gmail.com>
2023-05-03 03:17:30 +0300

    Add game functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,10 @@
 import "../styles/Card.css";
 
-export default function Card() {
-  return <div className="Card"></div>;
+export default function Card({ name, image, handleClick }) {
+  return (
+    <div className="Card" onClick={handleClick}>
+      <img src={image} alt={`${name}`} width={175} />
+      <p>{name}</p>
+    </div>
+  );
 }
--- src/components/CardHolder.js
+++ src/components/CardHolder.js
@@ -1,5 +1,64 @@
+import { useState } from "react";
 import "../styles/CardHolder.css";
+import { default as defaultCards } from "./cards";
+import Card from "./Card";
 
-export default function CardHolder() {
-  return <div className="Card-Holder"></div>;
+export default function CardHolder({ incrementScore, setHighScore }) {
+  const [cards, setCards] = useState(shuffleArray());
+
+  function shuffleArray(arr = defaultCards) {
+    const shuffledArray = [...arr];
+
+    for (let i = shuffledArray.length - 1; i > 0; i -= 1) {
+      let j = Math.floor(Math.random() * (i + 1));
+
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+
+    return shuffledArray;
+  }
+
+  function cardIsClicked(cardName) {
+    return cards.map((card) => {
+      if (cardName === card.name) {
+        return {
+          ...card,
+          clicked: true,
+        };
+      }
+      return card;
+    });
+  }
+
+  function handleClick(e) {
+    const cardName = e.currentTarget.lastChild.textContent;
+    const isClicked = cards.find((card) => card.name === cardName).clicked;
+    console.log();
+    if (!isClicked) {
+      incrementScore();
+      setCards(shuffleArray(cardIsClicked(cardName)));
+    } else {
+      setHighScore();
+      setCards(shuffleArray());
+    }
+  }
+
+  return (
+    <div className="Card-Holder">
+      {cards.map((card) => {
+        return (
+          <Card
+            name={card.name}
+            image={card.image}
+            clicked={card.clicked}
+            key={card.name}
+            handleClick={handleClick}
+          />
+        );
+      })}
+    </div>
+  );
 }
